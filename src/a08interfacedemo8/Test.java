a45d373058a95d8310126e3f7439e00c053a8954
Leon <212649354@qq.com>
2024-02-16 22:00:01 +0800

    JDK9以后接口中新增的私有方法

--- src/a08interfacedemo8/Test.java
+++ src/a08interfacedemo8/Test.java
@@ -2,6 +2,18 @@ package a08interfacedemo8;
 
 public class Test {
     public static void main(String[] args) {
+        /*接口中私有方法的定义格式：
+        格式1：private 返回值类型 方法名(参数列表){    }
+        范例1: private void show(){    }
 
+        格式2：private static 返回值类型 方法名(参数列表){    }
+        范例2: private static void method(){    }
+         */
+        /**
+         * 1.JDK7以前：接口中只能定义抽象方法。
+         * 2.JDK8：接口中可以定义有方法体的方法。（默认、静态）
+         * 3.JDK9：接口中可以定义私有方法。
+         * 4.私有方法分为两种：普通的私有方法，静态的私有方法
+         */
     }
 }
