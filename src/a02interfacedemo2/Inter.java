bd3065481cf62899a1b5efef1ce299f84ec17248
Leon <212649354@qq.com>
2024-02-16 18:31:30 +0800

    接口小细节1

---
+++ src/a02interfacedemo2/Inter.java
@@ -0,0 +1,9 @@
+package a02interfacedemo2;
+
+public interface Inter {
+    //public static final
+    public static final int a = 10;
+
+    //public abstract
+    public abstract void method();
+}
