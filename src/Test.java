274192749b8674494c3af65b39a29cc6fa8abebe
Leon <212649354@qq.com>
2024-02-16 14:46:44 +0800

    interface

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public static void main(String[] args) {
+        Dog d = new Dog("旺财", 3);
+        d.eat();
+        d.swim();
+        System.out.println(d.getName() + ", " + d.getAge());
+        System.out.println("------------------------");
+
+        Rabbit r = new Rabbit("玉兔", 2);
+        r.eat();
+        System.out.println(r.getName() + ", " + r.getAge());
+        System.out.println("------------------------");
+
+        Frog f = new Frog("青蛙", 4);
+        f.eat();
+        f.swim();
+        System.out.println(f.getName() + ", " + f.getAge());
+    }
+}
